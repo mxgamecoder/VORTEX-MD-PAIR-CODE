619fc07901be0acf443c46ea87f18cf50a6ccb88
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-11-30 20:15:15 +0100

    Add files via upload

--- qr.js
+++ qr.js
@@ -13,20 +13,23 @@ const fs = require("fs-extra");
 const { Boom } = require("@hapi/boom");
 
 const MESSAGE = process.env.MESSAGE ||  `
-*SESSION GENERATED SUCCESSFULY* ✅
+*SESSION GENERATED SUCCESSFULY* ☠️
 
-*Gɪᴠᴇ ᴀ ꜱᴛᴀʀ ᴛᴏ ʀᴇᴘᴏ ꜰᴏʀ ᴄᴏᴜʀᴀɢᴇ* 🌟
+*Gɪᴠᴇ ᴀ ꜱᴛᴀʀ ᴛᴏ ʀᴇᴘᴏ ꜰᴏʀ ᴄᴏᴜʀᴀɢᴇ* ☠️
 https://github.com/mxgamecoder/VORTEX-MD
 
-*Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ ꜰᴏʀ ϙᴜᴇʀʏ* 💭
+*Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ ꜰᴏʀ ϙᴜᴇʀʏ* ☠️
 https://t.me/mxgamecoder
+
+
+*Follow the VORTEX-BOT channel on WhatsApp* ☠️
 https://whatsapp.com/channel/0029Vavz0e6E50Ugp30Z6z0W
 
 
-*Yᴏᴜ-ᴛᴜʙᴇ ᴛᴜᴛᴏʀɪᴀʟꜱ* 🪄 
+*Yᴏᴜ-ᴛᴜʙᴇ ᴛᴜᴛᴏʀɪᴀʟꜱ* ☠️
 https://youtube.com/mxgamecoder
 
-*VORTEX-MD--WHATTSAPP-BOT* 🥀
+*VORTEX-MD--WHATSAPP-BOT* ☠️
 `
 
 
